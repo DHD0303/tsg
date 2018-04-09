4f34e7449018543685cf46b9c6c546d607b4320f
DHD0303 <1246009411@qq.com>
2018-04-09 23:20:32 +0800

    作业

---
+++ tsgs.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+    td {
+        width: 200px;
+        border: 1px solid black;
+        text-align: center;
+    }
+</style>
+<body>
+<form action="tsgs.php" method="post">
+    账号: <input name="username" id="username" type="text" placeholder="账号"/>
+    密码: <input name="password" id="password" type="password" placeholder="密码"/>
+    日期: <input name="date" id="date" type="text" placeholder="2017-01-01"/>
+    <input type="submit" value="查询"/>
+</form>
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$date = $_POST['date'];
+/**
+ * Created by PhpStorm.
+ * User: DHD
+ * Date: 2018/4/9
+ * Time: 12:04
+ */
+include "Snoopy.class.php";
+$snoopy =  new snoopy();
+$url = 'http://202.197.232.4:8081/opac_two/include/login_app.jsp';
+$history_url = 'http://202.197.232.4:8081/opac_two/reader/jieshulishi.jsp';
+$data = array('login_type'=>'',
+    'barcode'=>$username,
+    'password'=>$password);
+$snoopy->submit($url,$data);
+$res = $snoopy->results;
+$snoopy->setcookies();
+$snoopy->fetch($history_url);
+$history = $snoopy->results;
+$history = iconv("gbk", "utf-8", $history);
+$history = preg_replace("/\s|&nbsp|;/","", $history);
+$mode = '/<tdalign=left>(.*?)<\/td>/';
+$res = array();
+preg_match_all($mode, $history, $res);
+$i = 0;
+$history = array(array());
+foreach ($res[1] as $result) {
+    $history[$i/5][$i%5] = $result;
+    $i++;
+}
+//echo '<br><br><br>';
+//echo '<table>';
+//    echo '<tr><td></td><td>书名</td><td>图书编码</td><td>事件</td><td>日期</td></tr>';
+//foreach ($history as $result) {
+//    echo "<tr>";
+//    for($i=0; $i<5; $i++) {
+//        echo "<td>".$result[$i]."</td>";
+//    }
+//    echo "</tr>";
+//}
+//echo '</table>';
+$flag = 0;
+foreach ($history as $result) {
+    if( $result[4] == $date ) {
+        echo $result[4]."<br>".$result[3]."<br>".$result[1];
+        $flag = 1;
+        break;
+    }
+}
+if( $flag == 0 ) {
+    echo "这一天没有去图书馆";
+}
+?>
+</body>
+</html>
